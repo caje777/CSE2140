646bbc28131e6090f2c8218eb1cb4eb0b7130997
caje777 <160929942+caje777@users.noreply.github.com>
2024-05-01 10:27:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function makeProblem() {
   var firstNumber = Math.floor((Math.random() * 10) + 1);
   var secondNumber = Math.floor((Math.random() * 10) + 1);
   var chooseSign = ['+', '-', '*'][Math.floor(Math.random() * 3)];
-  var problem = 
+  var problemElement = document.getElementById('problem')
+  problemElement.textContent = firstNumber + ' ' + chooseSign + ' ' secondNumber
