3ffcef968a9354a78af5aaff493ea54021c614bf
caje777 <160929942+caje777@users.noreply.github.com>
2024-05-01 10:21:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function makeProblem() {
   var firstNumber = Math.floor((Math.random() * 10) + 1);
-  var SecondNumber = Math.floor((Math.random() * 10) + 1);
+  var secondNumber = Math.floor((Math.random() * 10) + 1);
   var chooseSign = ['+', '-', '*'][Math.floor(Math.random() * 3)];
   var problem = 
