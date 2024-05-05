a1a2cb85b7623de1c716e7e904cfbe5462390538
caje777 <160929942+caje777@users.noreply.github.com>
2024-05-04 18:57:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -79,5 +79,5 @@ function revealAnswer() {
   var problem = document.getElementById('problem').textContent;
   var result = eval(problem);
   var resultText = document.getElementById('resultText');
-  resultText.innerHTML = 'The correct Answer was ' + result;
+  resultText.revealedAnswer = 'The correct Answer was ' + result;
 }
